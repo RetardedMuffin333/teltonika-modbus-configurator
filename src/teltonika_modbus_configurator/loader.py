fa771ecb971c8735127e0a96008e4ec9457b258f
AljažM <115755807+RetardedMuffin333@users.noreply.github.com>
2026-09-18 14:17:57 +0200

    Serve Carel batches as block mappings

--- src/teltonika_modbus_configurator/loader.py
+++ src/teltonika_modbus_configurator/loader.py
@@ -154,6 +154,8 @@ def load_project(path: str | Path) -> Project:
         count=int(i.get("count", 1)),
         source_offset=int(i.get("source_offset", 0)),
         symbol_data_type=None if i.get("symbol_data_type") is None else str(i["symbol_data_type"]),
+        deploy=_bool(i.get("deploy"), True),
+        export_symbol=_bool(i.get("export_symbol"), True),
         source_id=None if i.get("source_id") is None else str(i["source_id"]),
     ) for i in raw_mappings]
 
